2804db1880bc69321a01b651f434406fdeca95bd
Aliaksandr Khramau <axelkhr@gmail.com>
2022-06-29 01:31:13 +0200

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let i = n.toString().length
+  let s = '';
+  while (i--) {
+    s += n.toString()[i];
+  }
+
+  return parseInt(s, 10);
 }
